c62c18f25bc3f8b09ea328eb8bfba9da2ddac548
MarceloUrsulino <marceloursulino40@gmail.com>
2026-06-22 22:10:31 -0300

    Criar conta - Accounts

--- 5-PROJ-ACCOUNTS/index.js
+++ 5-PROJ-ACCOUNTS/index.js
@@ -19,7 +19,9 @@ function operation() {
     ])
     .then((answer) => {
         const action = answer['action']
-        console.log(action)
+
+        if(action === 'Criar conta')
+            createAccount()
     })
     .catch((err) => console.log(err))
 }
@@ -28,5 +30,42 @@ function operation() {
 // create an account
 
 function createAccount(){
+    console.log(chalk.bgGreen.black('Parabéns por escolher nosso banco!'))
+    console.log(chalk.green('Defina as opções da sua conta a seguir.'))
+
+   buildAccount()
 
 }
+
+function buildAccount(){
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'Digite o nome da sua conta:',
+        }
+    ])
+    .then((answer) => {
+
+        const accountName = answer['accountName']
+
+        console.info(answer['accountName'])
+
+        if(!fs.existsSync('accounts')){
+            fs.mkdirSync('accounts')
+        }
+
+        if(fs.existsSync(`accounts/${accountName}.json`)){
+            console.log(chalk.bgRed.black('Esta conta já existe,escolha outro nome.'))
+
+            buildAccount()
+            return
+        }
+        fs.writeFileSync(`accounts/${accountName}.json`, '{"balance": 0}', function(err){
+            console.log(err)
+        })
+
+        console.log(chalk.green('Parabéns, a sua conta foi criada!'))
+        operation()
+    })
+    .catch((err) => console.log(err))
+}   
