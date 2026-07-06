961807da5424c5638b53a47d9c5c941c3cb24d95
MarceloUrsulino <marceloursulino40@gmail.com>
2026-07-05 21:01:40 -0300

    adicionei o arquivo .env para proteger senha do banco

--- 8-NODE-E-MYSQL/1-intalacao-mysql/index.js
+++ 8-NODE-E-MYSQL/1-intalacao-mysql/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express')
 const exphbs = require('express-handlebars')
 const mysql = require('mysql2')
@@ -14,8 +16,8 @@ app.get('/', (req, res) => {
 }) 
 
 const conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'nodemysql2'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 })
