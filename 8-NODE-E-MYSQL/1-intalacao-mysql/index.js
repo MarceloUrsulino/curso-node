0a413be2aba59b046229e798614dcb7f158b9eb6
MarceloUrsulino <marceloursulino40@gmail.com>
2026-07-06 22:24:26 -0300

    creating database

--- 8-NODE-E-MYSQL/1-intalacao-mysql/index.js
+++ 8-NODE-E-MYSQL/1-intalacao-mysql/index.js
@@ -1,12 +1,12 @@
 require('dotenv').config()
 
 const express = require('express')
-const exphbs = require('express-handlebars')
+const { engine } = require('express-handlebars')
 const mysql = require('mysql2')
 
 const app = express()
 
-app.engine('handlebars', exphbs())
+app.engine('handlebars', engine())
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
@@ -20,4 +20,13 @@ const conn = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
+})
+
+conn.connect(function(err){
+    if(err){
+        console.log(err)
+    }
+    console.log('Conectou ao Mysql.')
+
+    app.listen(3000)
 })
