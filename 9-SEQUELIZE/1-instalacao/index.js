93da107cf3120db3ef2c54b844e13e978d118a48
MarceloUrsulino <marceloursulino40@gmail.com>
2026-07-13 22:40:37 -0300

    Sequelize

---
+++ 9-SEQUELIZE/1-instalacao/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+
+const express = require('express')
+const { engine } = require('express-handlebars')
+const conn = require('./db/conn')
+
+const app = express()
+
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+app.use(express.json())
+
+
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+}) 
+
+ app.listen(3000)
