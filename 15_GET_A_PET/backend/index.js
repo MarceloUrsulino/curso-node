ca3e284d2da8eb6e2893a6cf74b28dae1f647c98
MarceloUrsulino <marceloursulino40@gmail.com>
2026-07-29 21:23:30 -0300

    Criando models GET A PET

--- 15_GET_A_PET/backend/index.js
+++ 15_GET_A_PET/backend/index.js
@@ -1,13 +1,15 @@
+require('dotenv').config() 
 const express = require('express')
 const cors = require('cors')
 
 const app = express()
 
+require('./db/conn')
 //config JSON response
 app.use(express.json())
 
 // Solve CORS
-app.use(cors({ credentials: true, origin: 'htpp://localhost:3000' }))
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
 
 //Public folder for images
 app.use(express.static('public'))
